19abed53490f9abdd38bdbb4a7117ccc52a3c81e
Adrian-Paul Carrières <adrian-paul.carrieres@myriadgroup.com>
2022-07-08 16:22:20 +0200

    delete inactive temp voice channels

--- lib/vox/consumer.ex
+++ lib/vox/consumer.ex
@@ -2,6 +2,8 @@ defmodule Vox.Consumer do
   @moduledoc false
   use Nostrum.Consumer
 
+  require Logger
+
   alias Vox.Commands
 
   def start_link do
@@ -14,6 +16,11 @@ defmodule Vox.Consumer do
 
   def handle_event({:INTERACTION_CREATE, interaction, _ws_state}) do
     Commands.handle_interaction(interaction)
+  end
+
+  def handle_event({:VOICE_STATE_UPDATE, %{channel_id: nil, guild_id: guild_id}, _ws_state}) do
+    Logger.metadata(guild_id: guild_id)
+    Vox.Utils.Voice.purge_inactive_voice_channels(guild_id)
   end
 
   def handle_event(_data) do
