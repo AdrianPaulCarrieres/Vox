d76b26ccb39b16ed262a8bd3903eda0ced1a03ac
Adrian-Paul Carrières <36892477+AdrianPaulCarrieres@users.noreply.github.com>
2022-09-03 19:04:00 +0200

    Put Elixir version back to 1.13 because of the lack of docker image for 1.14 :D

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Vox.MixProject do
   def project do
     [
       app: :vox,
-      version: "2.0.0",
-      elixir: "~> 1.14",
+      version: "2.0.1",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
