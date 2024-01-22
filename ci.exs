f65894a12053fec2f6c7b5094bf29f34f664c9c8
Adrian-Paul Carrières <36892477+AdrianPaulCarrieres@users.noreply.github.com>
2024-01-22 16:36:45 +0100

    ci

--- ci.exs
+++ ci.exs
@@ -5,6 +5,8 @@ Mix.install([
 defmodule Ci do
   alias Dagger.{Client, Container, Directory, Host}
 
+  require Logger
+
   @workdir "/app"
 
   def run do
@@ -15,30 +17,35 @@ defmodule Ci do
       |> Client.host()
       |> Host.directory(".")
 
-    deps_cache = Dagger.Client.cache_volume(client, "deps")
-    dev_cache = Dagger.Client.cache_volume(client, "_build/dev")
-    test_cache = Dagger.Client.cache_volume(client, "_build/test")
+    deps_cache = Client.cache_volume(client, "deps")
+    dev_cache = Client.cache_volume(client, "_build/dev")
+    test_cache = Client.cache_volume(client, "_build/test")
 
     source =
       client
       |> Client.container()
       |> Container.from("hexpm/elixir:1.15.6-erlang-26.1.1-debian-bullseye-20230612-slim")
       |> Container.with_workdir(@workdir)
-      |> Container.with_directory("config", Directory.directory(project, "/config"))
       |> Container.with_file("mix.exs", Directory.file(project, "mix.exs"))
       |> Container.with_file("mix.lock", Directory.file(project, "mix.lock"))
       |> Container.with_exec(~w"mix local.rebar --force")
       |> Container.with_exec(~w"mix local.hex --force")
       |> Container.with_mounted_cache("deps", deps_cache)
       |> Container.with_exec(~w"mix deps.get")
+      |> Container.with_directory("config", Directory.directory(project, "/config"))
       |> Container.with_directory(
         "lib",
         Directory.directory(project, "/lib")
       )
 
-    _hex_audit =
+    hex_audit =
       source
       |> Container.with_exec(~w"mix hex.audit")
+      |> Container.stdout()
+
+    unused_deps =
+      source
+      |> Container.with_exec(~w"mix deps.unlock --check-unused")
       |> Container.stdout()
 
     dev =
@@ -47,16 +54,17 @@ defmodule Ci do
       |> Container.with_mounted_cache("_build/dev", dev_cache)
       |> Container.with_exec(~w"mix compile")
 
-    doctor =
-      dev
-      |> Container.with_exec(~w"mix doctor")
-      |> Container.stdout()
-
     test =
       source
       |> Container.with_env_variable("MIX_ENV", "test")
       |> Container.with_mounted_cache("_build/test", test_cache)
       |> Container.with_exec(~w"mix compile")
+      |> Container.with_exec(~w"mix test")
+
+    doctor =
+      dev
+      |> Container.with_exec(~w"mix doctor")
+      |> Container.stdout()
 
     credo =
       dev
@@ -77,8 +85,7 @@ defmodule Ci do
 
     Dagger.close(client)
 
-    IO.inspect(credo, label: "credo")
-    IO.inspect(doctor, label: "doctor")
+    Logger.info("Everything is done!")
   end
 end
 
